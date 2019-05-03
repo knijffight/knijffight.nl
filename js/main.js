337936050dd2fa8063be98aeb748c404cca70f88
Midge 't Hoen <midgethoen@gmail.com>
2019-05-03 20:56:00 +0200

    Change lesson times, address and tweak instructors page

--- js/main.js
+++ js/main.js
@@ -5,113 +5,112 @@
 */
 
 (function($) {
+  skel.breakpoints({
+    xlarge: '(max-width: 1680px)',
+    large: '(max-width: 1280px)',
+    medium: '(max-width: 980px)',
+    small: '(max-width: 736px)',
+    xsmall: '(max-width: 480px)'
+  });
 
-	skel
-		.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+  $(function() {
+    var $window = $(window),
+      $body = $('body'),
+      $wrapper = $('#page-wrapper'),
+      $banner = $('#banner'),
+      $header = $('#header');
 
-	$(function() {
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
 
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#page-wrapper'),
-			$banner = $('#banner'),
-			$header = $('#header');
+    $window.on('load', function() {
+      window.setTimeout(function() {
+        $body.removeClass('is-loading');
+      }, 100);
+    });
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+    // Mobile?
+    if (skel.vars.mobile) $body.addClass('is-mobile');
+    else
+      skel
+        .on('-medium !medium', function() {
+          $body.removeClass('is-mobile');
+        })
+        .on('+medium', function() {
+          $body.addClass('is-mobile');
+        });
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+    // Fix: Placeholder polyfill.
+    $('form').placeholder();
 
-		// Mobile?
-			if (skel.vars.mobile)
-				$body.addClass('is-mobile');
-			else
-				skel
-					.on('-medium !medium', function() {
-						$body.removeClass('is-mobile');
-					})
-					.on('+medium', function() {
-						$body.addClass('is-mobile');
-					});
+    // Prioritize "important" elements on medium.
+    skel.on('+medium -medium', function() {
+      $.prioritize(
+        '.important\\28 medium\\29',
+        skel.breakpoint('medium').active
+      );
+    });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+    // Scrolly.
+    $('.scrolly').scrolly({
+      speed: 1500,
+      offset: $header.outerHeight()
+    });
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+    // Menu.
+    $('#menu')
+      .append('<a href="#menu" class="close"></a>')
+      .appendTo($body)
+      .panel({
+        delay: 500,
+        hideOnClick: true,
+        hideOnSwipe: true,
+        resetScroll: true,
+        resetForms: true,
+        side: 'right',
+        target: $body,
+        visibleClass: 'is-menu-visible'
+      });
 
-		// Scrolly.
-			$('.scrolly')
-				.scrolly({
-					speed: 1500,
-					offset: $header.outerHeight()
-				});
+    // Header.
+    if (skel.vars.IEVersion < 9) $header.removeClass('alt');
 
-		// Menu.
-			$('#menu')
-				.append('<a href="#menu" class="close"></a>')
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-menu-visible'
-				});
+    if ($banner.length > 0 && $header.hasClass('alt')) {
+      $window.on('resize', function() {
+        $window.trigger('scroll');
+      });
 
-		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
-
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
-
-				$window.on('resize', function() { $window.trigger('scroll'); });
-
-				$banner.scrollex({
-					bottom:		$header.outerHeight() + 1,
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
-
-			}
-
-	});
-
+      $banner.scrollex({
+        bottom: $header.outerHeight() + 1,
+        terminate: function() {
+          $header.removeClass('alt');
+        },
+        enter: function() {
+          $header.addClass('alt');
+        },
+        leave: function() {
+          $header.removeClass('alt');
+        }
+      });
+    }
+  });
 })(jQuery);
 
 function initMap() {
-	var home = {lat: 51.935402, lng: 4.350205};
-	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-		zoom: 11,
-		center: home,
-		disableDefaultUI: true,
-		scrollwheel: false,
-		styles: [{
-			stylers: [{ saturation: -30 }]
-		}]
-	});
-	var marker = new google.maps.Marker({
-		position: home,
-		map: map
-	});
+  var home = { lat: 51.9092098, lng: 4.3484847 };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 11,
+    center: home,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    styles: [
+      {
+        stylers: [{ saturation: -30 }]
+      }
+    ]
+  });
+  var marker = new google.maps.Marker({
+    position: home,
+    map: map
+  });
 }
