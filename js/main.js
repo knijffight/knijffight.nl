e185c8adc05a071a4429463cca42f345e9e2ccd2
Midge 't Hoen <midgethoen@gmail.com>
2020-05-13 20:47:56 +0200

    Add covid19 protocol

--- js/main.js
+++ js/main.js
@@ -4,61 +4,62 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
   skel.breakpoints({
-    xlarge: '(max-width: 1680px)',
-    large: '(max-width: 1280px)',
-    medium: '(max-width: 980px)',
-    small: '(max-width: 736px)',
-    xsmall: '(max-width: 480px)'
+    xlarge: "(max-width: 1680px)",
+    large: "(max-width: 1280px)",
+    medium: "(max-width: 980px)",
+    small: "(max-width: 736px)",
+    xsmall: "(max-width: 480px)",
   });
 
-  $(function() {
+  $(function () {
     var $window = $(window),
-      $body = $('body'),
-      $wrapper = $('#page-wrapper'),
-      $banner = $('#banner'),
-      $header = $('#header');
+      $body = $("body"),
+      $wrapper = $("#page-wrapper"),
+      $banner = $("#banner"),
+      $header = $("#header");
 
     // Disable animations/transitions until the page has loaded.
-    $body.addClass('is-loading');
+    $body.addClass("is-loading");
 
-    $window.on('load', function() {
-      window.setTimeout(function() {
-        $body.removeClass('is-loading');
+    $window.on("load", function () {
+      window.setTimeout(function () {
+        $body.removeClass("is-loading");
       }, 100);
     });
 
     // Mobile?
-    if (skel.vars.mobile) $body.addClass('is-mobile');
+    if (skel.vars.mobile) $body.addClass("is-mobile");
     else
       skel
-        .on('-medium !medium', function() {
-          $body.removeClass('is-mobile');
+        .on("-medium !medium", function () {
+          $body.removeClass("is-mobile");
         })
-        .on('+medium', function() {
-          $body.addClass('is-mobile');
+        .on("+medium", function () {
+          $body.addClass("is-mobile");
         });
 
     // Fix: Placeholder polyfill.
-    $('form').placeholder();
+    $("form").placeholder();
 
     // Prioritize "important" elements on medium.
-    skel.on('+medium -medium', function() {
+    skel.on("+medium -medium", function () {
       $.prioritize(
-        '.important\\28 medium\\29',
-        skel.breakpoint('medium').active
+        ".important\\28 medium\\29",
+        skel.breakpoint("medium").active
       );
     });
 
     // Scrolly.
-    $('.scrolly').scrolly({
+    $(".scrolly").scrolly({
       speed: 1500,
-      offset: $header.outerHeight()
+
+      offset: $header.outerHeight(),
     });
 
     // Menu.
-    $('#menu')
+    $("#menu")
       .append('<a href="#menu" class="close"></a>')
       .appendTo($body)
       .panel({
@@ -67,30 +68,30 @@
         hideOnSwipe: true,
         resetScroll: true,
         resetForms: true,
-        side: 'right',
+        side: "right",
         target: $body,
-        visibleClass: 'is-menu-visible'
+        visibleClass: "is-menu-visible",
       });
 
     // Header.
-    if (skel.vars.IEVersion < 9) $header.removeClass('alt');
+    if (skel.vars.IEVersion < 9) $header.removeClass("alt");
 
-    if ($banner.length > 0 && $header.hasClass('alt')) {
-      $window.on('resize', function() {
-        $window.trigger('scroll');
+    if ($banner.length > 0 && $header.hasClass("alt")) {
+      $window.on("resize", function () {
+        $window.trigger("scroll");
       });
 
       $banner.scrollex({
         bottom: $header.outerHeight() + 1,
-        terminate: function() {
-          $header.removeClass('alt');
+        terminate: function () {
+          $header.removeClass("alt");
         },
-        enter: function() {
-          $header.addClass('alt');
+        enter: function () {
+          $header.addClass("alt");
         },
-        leave: function() {
-          $header.removeClass('alt');
-        }
+        leave: function () {
+          $header.removeClass("alt");
+        },
       });
     }
   });
@@ -98,19 +99,19 @@
 
 function initMap() {
   var home = { lat: 51.9092098, lng: 4.3484847 };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+  var map = new google.maps.Map(document.getElementById("map-canvas"), {
     zoom: 11,
     center: home,
     disableDefaultUI: true,
     scrollwheel: false,
     styles: [
       {
-        stylers: [{ saturation: -30 }]
-      }
-    ]
+        stylers: [{ saturation: -30 }],
+      },
+    ],
   });
   var marker = new google.maps.Marker({
     position: home,
-    map: map
+    map: map,
   });
 }
